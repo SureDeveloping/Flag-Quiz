ab4fd30b266c71206321eca79ad2d2bcee16b9b3
Stephan Sure <stephan.sure@gmail.com>
2024-02-02 19:13:13 +0000

    Add startQuiz, rules and home function and connect them with the corresponding buttons

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/** Function starts the quiz by clicking on the corresponding button
+ */
+
+
+function startQuiz(link) {
+    location.href = "game.html";
+}
+
+function rules(link) {
+    location.href = "rules.html";
+}
+
+function home(link) {
+    location.href = "index.html";
+}
