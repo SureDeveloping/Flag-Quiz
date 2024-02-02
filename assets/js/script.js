41d4a002e96761fca27099ff62afae0091e66ea5
Stephan Sure <stephan.sure@gmail.com>
2024-02-02 19:16:40 +0000

    Add  Multi-line Comments to explain the functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 /** Function starts the quiz by clicking on the corresponding button
  */
-
-
 function startQuiz(link) {
     location.href = "game.html";
 }
 
+/** Function opens the rules page by clicking on the corresponding button
+ */
 function rules(link) {
     location.href = "rules.html";
 }
 
+/** Function opens the homepage, index.html by clicking on the corresponding button
+ */
 function home(link) {
     location.href = "index.html";
 }
