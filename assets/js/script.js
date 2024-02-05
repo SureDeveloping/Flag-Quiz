1bf04b2dc365a6de2a6f23e136f73442950fdbe0
Stephan Sure <stephan.sure@gmail.com>
2024-02-05 09:16:52 +0000

    Fix the if methode which count the flagNumber up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ let flagNumber = 0;
 function startQuiz(link) {
     loadFlags(flagNumber);
     loadAnswers(flagNumber);
-    if (flagNumber < quizLength) {
+    if (flagNumber =< quizLength) {
         flagNumber++;
     }
 }
@@ -15,7 +15,7 @@ function startQuiz(link) {
 function nextFlag() {
     loadFlags(flagNumber);
     loadAnswers(flagNumber);
-    if (flagNumber < quizLength) {
+    if (flagNumber =< quizLength) {
         flagNumber++;
     }
 }
